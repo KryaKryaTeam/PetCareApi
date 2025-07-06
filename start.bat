18b3c5bb552e2ede77c64b4186e020116568dbdf
Ivan <ivan.hryshchuk2011@gmail.com>
2025-07-06 18:26:27 +0300

    feat: enhance Docker setup and logging; add DEV_MODE prompt in start.bat, update command in compose.yaml, and log environment variables in index.ts

--- start.bat
+++ start.bat
@@ -1,1 +1,22 @@
-docker compose up --build
+@echo off
+echo Welcome to start helper!
+
+:: Prompt if it's a dev start
+:isDEV
+set /p is_dev=Is dev start? (Y/N): 
+
+if /i "%is_dev%"=="Y" (
+    set DEV_MODE=true
+) else if /i "%is_dev%"=="N" (
+    set DEV_MODE=false
+) else (
+    echo Invalid answer. Please enter Y or N.
+    goto isDEV
+)
+
+:: Start docker with current environment variables
+echo.
+echo Starting Docker Compose...
+docker compose up --no-attach mongo --build 
+
+pause
