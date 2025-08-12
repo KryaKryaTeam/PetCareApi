dfb5d2096ffbd81f6007cbecea1a5afedec9b38c
Ivan <ivan.hryshchuk2011@gmail.com>
2025-08-12 14:39:02 +0300

    test workflow

--- server/dist/utils/logger.js
+++ server/dist/utils/logger.js
@@ -12,21 +12,28 @@ class Logger {
         this.endpoint = endpoint;
         this.requestId = (0, id_generateor_1.generateId)("request");
         this.startDate = new Date();
+        this.service = null;
+        exports.globalLogger.set(this);
+    }
+    start() {
         if (process.env.DEV_MODE) {
             fs_1.default.appendFileSync(path_1.default.join(__dirname, "..", ".dev", `${this.requestId}.md`), `<style>.info-log{color: blue} .error-log{color: red} .debbug-log{color: green}</style> \n# [LOG FILE FOR REQUEST ${this.requestId}]`);
         }
-        this.info("Endpoint: " + endpoint);
+        if (!fs_1.default.existsSync(path_1.default.join(__dirname, "..", "logs")))
+            fs_1.default.mkdirSync(path_1.default.join(__dirname, "..", "logs"));
+        if (!fs_1.default.existsSync(path_1.default.join(__dirname, "..", "logs", "requests.log")))
+            fs_1.default.appendFileSync(path_1.default.join(__dirname, "..", "logs", "requests.log"), "");
+        this.info("Endpoint: " + this.endpoint);
         console.log(`REQUEST ${this.requestId} IS OPEN`);
-        exports.globalLogger.set(this);
     }
     info(message) {
-        this.writeMessage("[INFO] " + message, "info-log");
+        this.writeMessage("[INFO] " + message, "INFO");
     }
     error(message) {
-        this.writeMessage("[ERROR] " + message, "error-log");
+        this.writeMessage("[ERROR] " + message, "ERROR");
     }
     debbug(message) {
-        this.writeMessage("[DEBBUG] " + message, "debbug-log");
+        this.writeMessage("[DEBBUG] " + message, "DEBBUG");
     }
     end() {
         const time = new Date(new Date().getTime() - this.startDate.getTime());
@@ -35,27 +42,42 @@ class Logger {
         }
         console.log(`\n [REQUEST ENDED. TIME: ${time.getTime()}ms]`);
     }
-    writeMessage(message, class_) {
+    writeMessage(message, level) {
+        const class_level = {
+            INFO: "info-log",
+            ERROR: "error-log",
+            DEBBUG: "debbug-log",
+        };
         if (process.env.DEV_MODE) {
             fs_1.default.writeFileSync(path_1.default.join(__dirname, "..", ".dev", `${this.requestId}.md`), "\n - <span class='" +
-                class_ +
+                class_level[level] +
                 "'>" +
                 new Date().toTimeString() +
                 "   " +
                 message +
                 "</span>", { flag: "a" });
         }
+        else {
+            fs_1.default.writeFileSync(path_1.default.join(__dirname, "..", "logs", "requests.log"), JSON.stringify({
+                level,
+                data: new Date(),
+                message,
+                requestId: this.requestId,
+                endpoint: this.endpoint,
+                service: this.service || "none",
+            }) + "\n", { flag: "a" });
+        }
         console.log("\n -" + new Date().toTimeString() + "   " + message);
     }
 }
 exports.Logger = Logger;
 class GlobalLogger {
-    constructor() { }
     logger() {
         return this.logger_;
     }
     set(logger) {
         this.logger_ = logger;
+        this.logger_.start();
     }
 }
 exports.globalLogger = new GlobalLogger();
--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,77 +1,107 @@
-import path from "path";
-import { generateId } from "./id_generateor";
-import fs from "fs";
+import path from "path"
+import { generateId } from "./id_generateor"
+import fs from "fs"
+import axios from "axios"
 
 export class Logger {
-  endpoint: string;
-  requestId: string;
-  startDate: Date;
-  constructor(endpoint: string) {
-    this.endpoint = endpoint;
-    this.requestId = generateId("request");
-    this.startDate = new Date();
+    endpoint: string
+    requestId: string
+    startDate: Date
+    service: null | string
 
-    if (process.env.DEV_MODE) {
-      fs.appendFileSync(
-        path.join(__dirname, "..", ".dev", `${this.requestId}.md`),
-        `<style>.info-log{color: blue} .error-log{color: red} .debbug-log{color: green}</style> \n# [LOG FILE FOR REQUEST ${this.requestId}]`
-      );
+    constructor(endpoint: string) {
+        this.endpoint = endpoint
+        this.requestId = generateId("request")
+        this.startDate = new Date()
+        this.service = null
+
+        globalLogger.set(this)
+    }
+    start() {
+        if (process.env.DEV_MODE) {
+            fs.appendFileSync(
+                path.join(__dirname, "..", ".dev", `${this.requestId}.md`),
+                `<style>.info-log{color: blue} .error-log{color: red} .debbug-log{color: green}</style> \n# [LOG FILE FOR REQUEST ${this.requestId}]`
+            )
+        }
+
+        if (!fs.existsSync(path.join(__dirname, "..", "logs"))) fs.mkdirSync(path.join(__dirname, "..", "logs"))
+        if (!fs.existsSync(path.join(__dirname, "..", "logs", "requests.log")))
+            fs.appendFileSync(path.join(__dirname, "..", "logs", "requests.log"), "")
+
+        this.info("Endpoint: " + this.endpoint)
+
+        console.log(`REQUEST ${this.requestId} IS OPEN`)
+    }
+    info(message: string) {
+        this.writeMessage("[INFO] " + message, "INFO")
+    }
+    error(message: string) {
+        this.writeMessage("[ERROR] " + message, "ERROR")
+    }
+    debbug(message: string) {
+        this.writeMessage("[DEBBUG] " + message, "DEBBUG")
     }
 
-    this.info("Endpoint: " + endpoint);
+    end() {
+        const time = new Date(new Date().getTime() - this.startDate.getTime())
+        if (process.env.DEV_MODE) {
+            fs.writeFileSync(
+                path.join(__dirname, "..", ".dev", `${this.requestId}.md`),
+                `\n ## [REQUEST ENDED. TIME: ${time.getTime()}ms]`,
+                { flag: "a" }
+            )
+        }
 
-    console.log(`REQUEST ${this.requestId} IS OPEN`);
-
-    globalLogger.set(this);
-  }
-  info(message: string) {
-    this.writeMessage("[INFO] " + message, "info-log");
-  }
-  error(message: string) {
-    this.writeMessage("[ERROR] " + message, "error-log");
-  }
-  debbug(message: string) {
-    this.writeMessage("[DEBBUG] " + message, "debbug-log");
-  }
-  end() {
-    const time = new Date(new Date().getTime() - this.startDate.getTime());
-    if (process.env.DEV_MODE) {
-      fs.writeFileSync(
-        path.join(__dirname, "..", ".dev", `${this.requestId}.md`),
-        `\n ## [REQUEST ENDED. TIME: ${time.getTime()}ms]`,
-        { flag: "a" }
-      );
+        console.log(`\n [REQUEST ENDED. TIME: ${time.getTime()}ms]`)
     }
-    console.log(`\n [REQUEST ENDED. TIME: ${time.getTime()}ms]`);
-  }
-  writeMessage(message: string, class_?: string) {
-    if (process.env.DEV_MODE) {
-      fs.writeFileSync(
-        path.join(__dirname, "..", ".dev", `${this.requestId}.md`),
-        "\n - <span class='" +
-          class_ +
-          "'>" +
-          new Date().toTimeString() +
-          "   " +
-          message +
-          "</span>",
-        { flag: "a" }
-      );
+    writeMessage(message: string, level: "INFO" | "ERROR" | "DEBBUG" | "START" | "END") {
+        const class_level = {
+            INFO: "info-log",
+            ERROR: "error-log",
+            DEBBUG: "debbug-log",
+        }
+        if (process.env.DEV_MODE) {
+            fs.writeFileSync(
+                path.join(__dirname, "..", ".dev", `${this.requestId}.md`),
+                "\n - <span class='" +
+                    class_level[level] +
+                    "'>" +
+                    new Date().toTimeString() +
+                    "   " +
+                    message +
+                    "</span>",
+                { flag: "a" }
+            )
+        } else {
+            fs.writeFileSync(
+                path.join(__dirname, "..", "logs", "requests.log"),
+                JSON.stringify({
+                    level,
+                    data: new Date(),
+                    message,
+                    requestId: this.requestId,
+                    endpoint: this.endpoint,
+                    service: this.service || "none",
+                }) + "\n",
+                { flag: "a" }
+            )
+        }
+        console.log("\n -" + new Date().toTimeString() + "   " + message)
     }
-    console.log("\n -" + new Date().toTimeString() + "   " + message);
-  }
 }
 
 class GlobalLogger {
-  private logger_!: Logger;
-  constructor() {}
-  logger(): Logger {
-    return this.logger_;
-  }
+    private logger_!: Logger
 
-  set(logger: Logger): void {
-    this.logger_ = logger;
-  }
+    logger(): Logger {
+        return this.logger_
+    }
+
+    set(logger: Logger): void {
+        this.logger_ = logger
+        this.logger_.start()
+    }
 }
 
-export const globalLogger = new GlobalLogger();
+export const globalLogger = new GlobalLogger()
